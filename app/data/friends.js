0c0cfbc8d2c7388d74ec17d7932efe49d5421f94
unknown <jniederer7@gmail.com>
2017-11-28 20:53:32 -0500

    final push

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = {
+  friendsArray :[
+    {
+      "name":"Justin",
+      "scores":[
+         5,
+         1,
+         4,
+         4,
+         5,
+         1,
+         2,
+         5,
+         4,
+         1
+
+          ]
+    }
+  ],
+  addFriend: function(friend){
+    this.friendsArray.push(friend);
+  }
+}
